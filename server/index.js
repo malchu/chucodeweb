e862148e05abaee579f212e9c4f8eadcd1816547
malchu <malchupascual@gmail.com>
2023-07-08 18:32:53 -0400

    routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const ProblemModel = require("./models/problems");
+import router from "./Routes/routes.js";
 
 const cors = require("cors");
 const { response } = require("express");
@@ -15,20 +16,32 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
-mongoose.connect("mongodb+srv://malchu:malchu@chucode.e7ldrkb.mongodb.net/chucode?retryWrites=true&w=majority").then(() => {
-  const PORT= process.env.PORT || 8000
-  app.listen(PORT, () => {
-    console.log("Malchu");
-  });
-}).catch(err => {
-  console.log(err);
-});
-
-app.set('view engine', 'html');
-
 app.get("/", (req, res) => {
-  res.render('index');
+  res.send({ message: "Hello World!" });
 });
+
+app.use("/api/", router);
+
+const startServer = async () => {
+  try {
+      mongoose.connect("mongodb+srv://malchu:malchu@chucode.e7ldrkb.mongodb.net/chucode?retryWrites=true&w=majority").then(() => {
+        const PORT= process.env.PORT || 8000
+        app.listen(PORT, () => {
+          console.log("Malchu");
+        });
+      }).catch(err => {
+        console.log(err);
+      });
+
+      app.listen(8080, () =>
+          console.log("Server started on port http://localhost:8080"),
+      );
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+startServer();
 
 app.get("/getProblems", (req, res) => {
     ProblemModel.find({}, (err, result) => {
